fcc36adf1f980f1c60bf2c4572165e167eca41a4
pravn27 <praveen.sri27@gmail.com>
2025-02-11 22:56:25 +0530

    initila commit

---
+++ auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWpLJqM_HyYvJFIgL7PEok4en-eCkzX4g",
+  authDomain: "auth-gp-b2dee.firebaseapp.com",
+  projectId: "auth-gp-b2dee",
+  storageBucket: "auth-gp-b2dee.firebasestorage.app",
+  messagingSenderId: "950694224139",
+  appId: "1:950694224139:web:3531f110a77cbe9a84820e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
